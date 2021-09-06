ebbea8617b07f8b6f45b8f91271536557a5ede49
davide <davide.baiocchi97@gmail.com>
2021-09-06 15:21:03 +0200

    end header

---
+++ project/resources/views/comics.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Comics')
+
+<!-- qui inserirò i conenuti della pagina esclusi le parti fisse per tutto il sito -->
+@section('content')
+    <h1>
+        inserire la parte della Comics
+    </h1>
+@endsection
---
+++ project/resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div>
+        sign-up now!
+    </div>
+    <div>
+        follow us
+    </div>
+</footer>
