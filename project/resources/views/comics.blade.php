6fff7e3d333dc554a4bf6fb99416aca2ca2ff7bb
davide <davide.baiocchi97@gmail.com>
2021-09-06 17:01:17 +0200

    end album

--- project/resources/views/comics.blade.php
+++ project/resources/views/comics.blade.php
@@ -3,16 +3,29 @@
 
 <!-- qui inserirò i conenuti della pagina esclusi le parti fisse per tutto il sito -->
 @section('content')
+
     <div class="jumbotron">
 
     </div>
+
     <span class="x">
         current series
     </span>
+
     <div class="albums">
         @foreach($albums as $album)
         <div class="card">
-            <img src="{{ $album['thumb']}}" alt="{{ $album['title']}}">
-            <p> {{ $album['title'] }} </p>
+            <img src=" {{ $album['thumb']}}" alt="{{ $album['series']}} ">
+            <p> {{ $album['series'] }} </p>
         </div>
+        @endforeach
+
+        <span class="y">
+            load more
+        </span>
+    </div>
+
+    <div class="row-blue">
+        
+    </div>
 @endsection
