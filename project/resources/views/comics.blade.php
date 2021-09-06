41da7e8d3ed80c489a04c5d1b3fa1aadc7dd1b40
davide <davide.baiocchi97@gmail.com>
2021-09-06 15:50:30 +0200

    try bg-image

--- project/resources/views/comics.blade.php
+++ project/resources/views/comics.blade.php
@@ -4,6 +4,6 @@
 <!-- qui inserirò i conenuti della pagina esclusi le parti fisse per tutto il sito -->
 @section('content')
     <div class="jumbotron">
-        <img src=" {{ asset('img/jumbotron.jpg') }} " alt="">
+        
     </div>
 @endsection
